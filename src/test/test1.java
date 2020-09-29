c939d90a9ff3785e3c264edc958e0b48e110425f
user <user@EC01>
2020-09-29 09:22:03 +0800

    test1-2

--- src/test/test1.java
+++ src/test/test1.java
@@ -4,7 +4,7 @@ public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello 123");
+		System.out.println("Hello 1234");
 	}
 
 }
